8ba6e61500e842272c1381a630675d10b045d2e1
darekszyper <114878453+darekszyper@users.noreply.github.com>
2024-07-21 14:06:08 +0200

    task04

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -2,24 +2,42 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent.SNSRecord;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@LambdaHandler(lambdaName = "sns_handler",
-	roleName = "sns_handler-role",
-	isPublishVersion = false,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+//@SnsEventSource - the annotation which allows to subscribe a Lambda to an SNS topic:
+//
+//targetTopic* - the name of an SNS topic to which the Lambda function is subscribed.
+//regionScope – the region or regions in which the SNS topic is located. The default value
+//RegionScope.DEFAULT means that the Lambda is deployed only to the SNS topic in the regions specified in the config during deploy.
+@LambdaHandler(
+        lambdaName = "sns_handler",
+        roleName = "sns_handler-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
+@SnsEventSource(
+        targetTopic = "lambda_topic"
+)
+public class SnsHandler implements RequestHandler<SNSEvent, Void> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
-	}
+    @Override
+    public Void handleRequest(SNSEvent snsEvent, Context context) {
+        for (SNSRecord msg : snsEvent.getRecords()) {
+            sendMessageToCloudWatchLogs(msg, context);
+        }
+        return null;
+    }
+
+    private void sendMessageToCloudWatchLogs(SNSRecord msg, Context context) {
+        try {
+            context.getLogger().log(msg.toString());
+        } catch (Exception e) {
+            context.getLogger().log("An error occurred");
+            throw e;
+        }
+    }
 }
